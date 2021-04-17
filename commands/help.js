d7401ee07b1d67908d89e3275298a3a781463fb5
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-04-17 20:29:02 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,14 +7,14 @@ i18n.setLocale(LOCALE);
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: ("clip,clips,help,invite,loop,lyrics,move,np,pause,ping,play,playlist,pruning,queue,remove,search,suffle,skip,skipto,stop,uptime,volume"),
+  description: ("24/7,join,disconnect,clip,clips,help,invite,loop,lyrics,move,np,pause,ping,play,playlist,pruning,queue,remove,search,suffle,skip,skipto,stop,uptime,volume"),
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
       .setTitle(i18n.__mf("help.embedTitle", { botname: message.client.user.username }))
       .setDescription(i18n.__("clip,clips,help,invite,loop,lyrics,move,np,pause,ping,play,playlist,pruning,queue,remove,search,suffle,skip,skipto,stop,uptime,volume"))
-      .setColor("#F8AA2A");
+      .setColor("EMBED_COLOR");
 
     commands.forEach((cmd) => {
       helpEmbed.addField(
