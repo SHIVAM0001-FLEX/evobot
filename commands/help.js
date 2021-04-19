3201e6f8bc959fe0a0b6ccf0d251473841e7ba73
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-04-19 16:23:39 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,12 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: ("24/7,join,disconnect,clip,clips,help,invite,loop,lyrics,move,np,pause,ping,play,playlist,pruning,queue,remove,search,suffle,skip,skipto,stop,uptime,volume"),
+  description: ("24/7,join,disconnect,clip,clips
+                help,invite,loop,lyrics,move
+                np,pause,ping,play,playlist
+                pruning,queue,remove,search
+                suffle,skip,skipto,stop,uptime
+                volume"),
   execute(message) {
     let commands = message.client.commands.array();
 
