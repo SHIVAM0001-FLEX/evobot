c0edb5280f6b0707bf2015cbef1c3b68d5fc3556
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-04-19 08:55:23 +0530

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -9,18 +9,18 @@ module.exports = {
         const { channel } = message.member.voice;
 
         const novc = new MessageEmbed()
-        .setDescription(`You need to join a voice channel first!`)
-        .setColor("YELLOW");
+        .setDescription(`:wrong: You need to join a voice channel first!`)
+        .setColor("RED");
 
         const joinn = new MessageEmbed()
-        .setDescription(":sez_music_correct: 24/7 mode is now **Enabled** in this server.")
-        .setColor("YELLOW");
+        .setDescription(":right: 24/7 mode is now **Enabled** in this server.")
+        .setColor("RED");
 
         const serverQueue = message.client.queue.get(message.guild.id);
         if (!channel) return message.channel.send(novc).catch(console.error);
         const nosamechannel = new MessageEmbed()
-        .setDescription(` You must be in the same channel as ${message.client.user}`)
-        .setColor("YELLOW");
+        .setDescription(`:wrong:You must be in the same channel as ${message.client.user}`)
+        .setColor("RED");
         message.channel.send(joinn)
         
 
